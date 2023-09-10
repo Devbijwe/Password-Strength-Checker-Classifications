d73ede8e8170812744cfa0d7c5a58d40b692899c
Devbijjwe <22devendrabijwe@gmail.com>
2023-09-10 12:12:27 +0530

    Updated

--- Password Strength Checker.py
+++ Password Strength Checker.py
@@ -1,20 +1,14 @@
 import pandas as pd
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-import joblib
-import getpass
-
 
 def word(password):
-    character=[]
+    character = []
     for i in password:
         character.append(i)
     return character
-  
-
 
 def preprocess_and_predict():
     data = pd.read_csv("password_strength.csv", error_bad_lines=False)
@@ -33,23 +27,12 @@ def preprocess_and_predict():
                                                     test_size=0.2, 
                                                     random_state=42)
         
-   
-    
-    # Assuming you have xtrain, ytrain, xtest, and ytest data
+
     model = RandomForestClassifier()
-    loaded_model=model.fit(xtrain, ytrain)
-
-    # # Save the trained model to a file
-    # filename = 'random_forest_model.pkl'
-    # joblib.dump(model, filename)
-
-    # # Load the saved model from the file
-    # loaded_model = joblib.load(filename)
+    loaded_model = model.fit(xtrain, ytrain)
 
     # Use the loaded model for predictions
     score = loaded_model.score(xtest, ytest)
-   
-    tdif = TfidfVectorizer()  # You need to initialize and fit_transform your TD-IDF vectorizer here
 
     # Get user input
     user = input("Enter Text: ")
@@ -61,9 +44,6 @@ def preprocess_and_predict():
     output = loaded_model.predict(data)
     return output
 
-
-if __name__=="__main__":
-    
-    result=preprocess_and_predict()
+if __name__ == "__main__":
+    result = preprocess_and_predict()
     print(result)
-            
